edd17638f3619a6a801f243f14e74648970df85b
Eve Amber Goodmon <eveambergoodmon@Eves-MacBook-Air.local>
2022-09-05 19:06:24 +0700

    Memodifikasi folder 08-Latihan 1 - ALP 3832 dan mengganti nama file Main.out jadi Kelompok 1 Lego dan Salfa

---
+++ Soal Kuliah/08-Latihan 1 - ALP 3832  /src/Kelompok 1 Lego dan Salfa.cpp
@@ -0,0 +1,33 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main () {
+
+    //Judul 
+    cout << "====== Indentitas Sederhana ======\n\n" << endl;
+
+    //Variable yang dibutuhkan 
+    char initial = 's';
+    int tahun_lahir, tahun_saat_ini = 2022;
+
+    //Memasukan pertanyaan beserta inputnya
+    cout << "Masukan inisial nama kamu : ";
+    cin >> initial;
+    cout << "Maukan tahun lahir kamu   : ";
+    cin >> tahun_lahir;
+
+    //Rumus umur
+    int umur = tahun_saat_ini - tahun_lahir;
+
+    cout << "\n----------------------------------\n" << endl;
+
+    //Meng-output kan semua input
+    cout << "Halo \"" << initial << "\"! Tahun ini kamu berumur " << umur << " tahun \\^.^/" << endl;
+
+    cout << endl;
+
+    return 0;
+
+}
